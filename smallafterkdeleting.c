f9e5e4481033583a27b9b6e00b1b0c26e4f90abd
saichinnu <venkatasai.573@gmail.com>
2018-07-05 09:04:35 -0700

    smallest Number After Deleting K digits in an Number

---
+++ smallafterkdeleting.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+    int num,i,j,a[10],n=0,temp=0,k;
+    scanf("%d",&num);
+    scanf("%d",&k);
+    while(num!=0)
+    {
+        a[n]=num%10;
+        num=num/10;
+        n++;
+    }
+    
+    for(i=0;i<n;i++)
+    {
+      for(j=i+1;j<n;j++)
+          { 
+              if(a[i]>a[j])
+               {
+                 temp=a[i];
+                 a[i]=a[j];
+                 a[j]=temp;}
+               }
+           temp=0;
+    }
+    
+    for(i=0;i<n-k;i++)
+    {
+       temp=(temp*10)+a[i];
+    }
+    printf("%d",temp);
+  
+}
