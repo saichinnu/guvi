e140a7a0e7703d3b89d98c68166873391054a965
saichinnu <venkatasai.573@gmail.com>
2018-07-06 07:30:11 -0700

    Create sumsquare.c
    Sum of Squares in a Number

---
+++ sumsquare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+   long int n,a,s=0;
+   scanf("%ld",&n);
+   if(n>=1&&n<=1000000000000000000)
+   {
+   while(n!=0)
+   {
+      a=n%10;
+      s=s+(a*a);
+      n=n/10;  
+   }
+   printf("%ld",s);
+   }
+   else
+   printf("Invalid Input");
+}
