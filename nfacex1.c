75b20b32d189dab70b2576cfc6a2a875193c9b36
saichinnu <venkatasai.573@gmail.com>
2018-07-08 11:34:15 -0700

    Create nfacex1.c

---
+++ nfacex1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  long int n,i,c=0;
+  scanf("%ld",&n);
+  for(i=2;i<n;i++)
+  {
+     if(n%i==0)
+     c++;
+  }
+   if(c>0)
+   printf("yes");
+   else
+   printf("no");
+   return 0;
+}
