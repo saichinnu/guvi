660af4bcd9aeac8d2a5130b324bfbbfbe84c5031
saichinnu <venkatasai.573@gmail.com>
2018-07-10 11:41:53 -0700

    Create grampan.c

---
+++ grampan.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    int i,used[26]={0},total=0;
+    gets(s);
+    for(i=0;s[i]!='\0';i++)
+    {
+        if('a'<=s[i]&&s[i]<='z')
+        {
+            total+=!used[s[i]-'a'];
+            used[s[i]-'a']=1;
+        }
+        else if('A'<=s[i]&&s[i]<='Z')
+        {
+            total+=!used[s[i]-'A'];
+            used[s[i]-'A']=1;
+        }
+    }
+    if(total==26)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
