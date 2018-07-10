9a4dd9d146ab176b967e8fff00878acd619cfc1d
saichinnu <venkatasai.573@gmail.com>
2018-07-10 11:15:48 -0700

    Create minnum.c

---
+++ minnum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+        int n,a[10],i,j,s;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	s=a[0];
+	for(i=0;i<n-1;i++)
+        {
+           if(a[i]>a[i+1])
+           s=a[i+1];
+        }
+    printf("%d",s);
+    return 0;
+}
