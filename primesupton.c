f175d805c0e666ddd049d696b04eebd1314672f7
saichinnu <venkatasai.573@gmail.com>
2018-07-11 08:18:46 -0700

    Create primesupton.c

---
+++ primesupton.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+   int c=0,i,b=0,m,n;
+   scanf("%d",&n);
+   for(m=1;m<=n;m++)
+   {
+   for(i=1;i<=m;i++)
+   {
+       if(m%i==0)
+       {
+         c++;
+       }
+   }
+     if(b==0)
+     { 
+     if(c==2)
+     {
+     printf("%d",m);
+     b++;
+     }
+     }
+     else
+     {
+     if(c==2)
+     printf(" %d",m);  
+     }
+     m++;
+     c=0;
+   }
+   return 0;
+}
