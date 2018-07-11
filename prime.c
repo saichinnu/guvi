9c22422133dcb379ca393aa0cd5c341b981938cd
saichinnu <venkatasai.573@gmail.com>
2018-07-10 23:34:31 -0700

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-void main()
+int main()
 {
    int b=0,i,n;
    scanf("%d",&n);
-   if(n<=1000)
-   {
    for(i=1;i<=n;i++)
    {
        if(n%i==0)
@@ -16,7 +14,5 @@ void main()
    printf("yes");
    else
    printf("no");
-   }
-   else
-   printf("invalid input");
+   return 0;
 }
