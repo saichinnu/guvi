e43ffb86edc70f4b21b51c56006a9c78b4b618da
saichinnu <venkatasai.573@gmail.com>
2018-07-10 23:12:09 -0700

    Update greatest.c

--- greatest.c
+++ greatest.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main()
+int main()
 {
    int a,b,c;
    scanf("%d%d%d",&a,&b,&c);
@@ -9,4 +9,5 @@ void main()
    printf("%d",b);
    else
    printf("%d",c);
+   return 0;
 }
