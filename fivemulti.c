20485459faedc207862e8e4f3cb9f2416ac51507
saichinnu <venkatasai.573@gmail.com>
2018-07-10 23:44:57 -0700

    Update fivemulti.c

--- fivemulti.c
+++ fivemulti.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
-void main()
+int main()
 {
    int n,i,a;
    scanf("%d",&n);
    for(i=1;i<=5;i++)
    {
      a=n*i;
-     printf("%d ",a);
+     if(i==1)
+     printf("%d",a);
+     else
+     printf(" %d",a);
    }
+   return 0;
 }
