ac4a57671ac73282a34f9e9689442aa3c151f768
saichinnu <venkatasai.573@gmail.com>
2018-07-12 23:55:49 -0700

    Update digipalindrome.c

--- digipalindrome.c
+++ digipalindrome.c
@@ -1,31 +1,8 @@
 #include<stdio.h>
 int main()
 { 
-   int n,i,a,b,s=0,d=0;
-   scanf("%d",&n);
-   while(n!=0)
-   { 
-      a=n%10;
-      s=s+a;
-      n=n/10;
-   }
-   b=s;
-   while(s!=0)
-   {
-      a=s%10;
-      d=(d*10)+a;
-      s=s/10;  
-   }
-   if(d==b)
-   printf("YES");
-   else
-   printf("NO");
-   return 0;
-}#include<stdio.h>
-int main()
-{ 
-   int n,i,a,b,s=0,d=0;
-   scanf("%d",&n);
+   long int n,i,a,b,s=0,d=0;
+   scanf("%ld",&n);
    while(n!=0)
    { 
       a=n%10;
