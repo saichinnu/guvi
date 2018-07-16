2cb573c78e73b2ca30e7b644aa9390f9e443e1bc
saichinnu <venkatasai.573@gmail.com>
2018-07-16 02:37:21 -0700

    Update sumxplus.c

--- sumxplus.c
+++ sumxplus.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     int n,i;
     scanf("%d",&n);
-    
     for(i=0;i<=n;i++)
     {
       if(i+ss(i)==n)
       { 
          printf("1\n");
-         printf("%d",i);    
+         printf("%d",i);   
+         exit(0);
       }
     }
+    printf("0");
     return 0;
 }
 int ss(int g)
