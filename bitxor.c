ef33b61e3398d9ab17dac1885c5d6ac67631b324
saichinnu <venkatasai.573@gmail.com>
2018-07-18 01:07:26 -0700

    Update bitxor.c

--- bitxor.c
+++ bitxor.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
 int main()
 {
-  int n,a[30],i,d=0;
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-  scanf("%d",&a[i]);
-  for(i=0;i<n;i++)
-  {
-     d=d^a[i];    
-  }
+  int n,i,d;
+  scanf("%d%d",&n,&i);
+  d=n&i;
   printf("%d",d);
   return 0;
 }
