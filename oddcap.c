8189bd7f7d23533a57e4ed0834fbd773f15c919b
saichinnu <venkatasai.573@gmail.com>
2018-09-21 11:27:14 -0700

    Update oddcap.c

--- oddcap.c
+++ oddcap.c
@@ -6,10 +6,18 @@ int main()
   int i,n;
   gets(a);
   n=strlen(a);
-  for(i=0;i<=n;i=i+2)
-  {
+  for(i=0;i<n;i=i+2)
+  { 
+     if(i!=0)
+     {
+       if(a[i-1]==' ')
+       {
+       	i++;
+       }
+     }
      if(a[i]!=' ')
      {
+       if(a[i]>='a'&&a[i]<='z')
        a[i]=a[i]-32; 
      }
      if(a[i]==' ')
