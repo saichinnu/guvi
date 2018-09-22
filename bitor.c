9c5545d91c651138d95f36e04b3cd49e4d6c6b4c
saichinnu <venkatasai.573@gmail.com>
2018-09-21 21:39:45 -0700

    Create bitor.c

---
+++ bitor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+  int n,a[50],i,c;
+  scanf("%d",&n);
+  for(i=0;i<n;i++) 
+  scanf("%d",&a[i]);
+  c=a[0];
+  for(i=1;i<n;i++) 
+  {
+     c=c|a[i];
+  }
+  printf("%d",c);
+  return 0;
+}
